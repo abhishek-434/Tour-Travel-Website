b3ab4e98576bf106ed9886808f1059c881e8b4f9
abhishek-434 <abhishekkumar2882003@gmail.com>
2026-07-27 23:36:10 +0530

    fix issue

--- tour_travel/settings.py
+++ tour_travel/settings.py
@@ -135,7 +135,11 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
